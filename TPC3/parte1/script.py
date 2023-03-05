bb1ee1610bcef883c1d54336ffb34bc9605e506a
MiguelCidadeSilva <mikefsi@hotmail.com>
2023-03-05 18:34:57 +0000

    Adaptei o codigo de gerar ids para trabalhar com os datasets todos fornecidos, acrescentei butões de retorno nas paginas html geradas.

--- TPC3/parte1/script.py
+++ TPC3/parte1/script.py
@@ -1,12 +1,21 @@
 import json
 
-f =  open("dataset-extra1.json",encoding="utf8")
-dicionario = json.load(f)
-pessoas = dicionario['pessoas']
+ficheiros = ["dataset-extra1.json","dataset-extra2.json","dataset-extra3.json","dataset.json"]
+dicionarioTotal = {}
 id = 0
-for pessoa in pessoas:
-    pessoa['id'] =  f"p{id}"
-    id+=1
 
-with open("dataset-extra1.json", "w") as f:
-    json.dump(dicionario,f,indent=4)
+for ficheiro in ficheiros:
+    with open(ficheiro, encoding="utf8") as f:
+        dicionario = json.load(f)
+    pessoas = dicionario['pessoas']
+    for pessoa in pessoas:
+        pessoa['id'] = f"p{id}"
+        id += 1
+    for key, value in dicionario.items():
+        if key == "pessoas":
+            dicionarioTotal[key] = dicionarioTotal.get(key, []) + value
+        else:
+            dicionarioTotal[key] = value
+
+with open("dataset-res.json", "w") as f:
+    json.dump(dicionarioTotal,f,indent=4)
